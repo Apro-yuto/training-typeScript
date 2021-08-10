7f8b8e9c50f89dc7cc30f249aca652a756c02599
Yuto <youto_drum@yahoo.co.jp>
2021-08-10 17:21:11 +0900

    Add: 軽く設計を書いた。1, 2番のプログラム完了。

--- src/assets/js/sample.ts
+++ src/assets/js/sample.ts
@@ -1,6 +1,41 @@
 // yuto nagashima  @@@@@@ https://github.com/Apro-yuto @@@@@@
 
+// 1, 今の天気をアイコンで表示
+// 2, 今日は雨が降るのかを表示。降るのであれば、何時頃か(テキスト)  例) 今日は降らないよ！ 例) 19時頃に雨が降るよ！
+// 3, 12時の気温と20時の気温を、「昼」、「夜」と横並びで気温を表示。
+// 4, 警報があれば。背景赤で表示。
+
 import axios from "axios";
+
+interface axiosOpt {
+  floorLat: number
+  floorLong: number
+}
+
+interface weatherAPIToday {
+  "dt": number,
+  "temp": number,
+  "num"?: number,
+  "feels_like": number,
+  "pressure": number,
+  "humidity": number,
+  "dew_point": number,
+  "uvi": number,
+  "clouds": number,
+  "visibility": number,
+  "wind_speed": number,
+  "wind_deg": number,
+  "wind_gust": number,
+  "weather": [
+      {
+          "id": number,
+          "main": string,
+          "description": string,
+          "icon": string
+      }
+  ],
+  "pop": number
+}
 
 class CurrentLocation {
   static getCurrentLocation(): Promise<GeolocationPosition> {
@@ -57,17 +92,59 @@ CurrentLocation.getCurrentLocation().then( result => {
   console.log(err)
 })
 
-interface axiosOpt {
-  floorLat: number
-  floorLong: number
-}
-
 const axiosGet = (url: string, opt:axiosOpt): void=> {
   const APIKEY: string = 'bb0e12659550392b8b3ca22b7089a50f'
-  axios.get(`${url}?lat=${opt.floorLat}&lon=${opt.floorLong}&appid=${APIKEY}`)
+  axios.get(`${url}?lat=${opt.floorLat}&lon=${opt.floorLong}&appid=${APIKEY}&units=metric&lang=ja`)
   .then( (res: any) => {
-    console.log({res})
+
+    const rainyID: number = 622;
+
+    // const testID: number = 622
+
+    const currentData: weatherAPIToday = res.data.current;
+    const todayData: weatherAPIToday[] = res.data.hourly.slice(0,24);
+
+
+    // 1, 今の天気をアイコンで表示
+    getWeatherIcon(currentData);
+
+
+    const checkRainy: weatherAPIToday[] = todayData.filter( (data: any, num:number, array: weatherAPIToday[] ) => {
+      data['num'] = num
+      console.log({data, num, array})
+      return data.weather[0].id < rainyID
+    } )
+
+    const rainyText =  checkRainy.length > 0 ? `${checkRainy[0].num}時間後に雨が降るよ！` : '今日は雨降らないよ！'
+    console.log({res,rainyText, checkRainy,currentData})
+
   })
 }
+
+// 1, 今の天気をアイコンで表示 -- script
+
+function getWeatherIcon(currentData: weatherAPIToday): void {
+  const sunnyID: number = 800;
+  const cloudyID: number = 801;
+
+  let iconPath: string | undefined = undefined;
+
+  if(currentData.weather[0].id === sunnyID) {
+    iconPath = 'img_sun.png'
+  } else if(currentData.weather[0].id >= cloudyID) {
+    iconPath = 'img_cloud.png'
+  } else {
+    iconPath = 'img_rainy.png'
+  }
+
+  inputDOM('weather_icon', iconPath)
+}
+
+// DOM操作
+function inputDOM(id: string, inputTxt: string): void {
+  const $DOM: HTMLElement = document.getElementById(id)!;
+  $DOM.innerHTML = inputTxt;
+}
+
 
 // yuto nagashima  @@@@@@ https://github.com/Apro-yuto @@@@@@
